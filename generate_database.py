e2348e22a085fdcd48f3e1924b2f591a46d8c957
Sebastian Medrano <sebastian.medrano@fundacion-jala.org>
2019-03-30 04:16:34 -0400

    data added to the tables in the database

--- generate_database.py
+++ generate_database.py
@@ -4,6 +4,8 @@ from typing import Dict
 
 from backend.city.city import City
 from backend.department.department import Department
+from backend.disaster.disaster import Disaster
+from backend.protocol.protocol import Protocol
 from database.database import Database
 from database.table_generator.table_generator import TableGenerator
 from database.query_launcher import QueryLauncher
@@ -34,6 +36,30 @@ def generate(config: Dict) -> None:
     data = {'user_name': '', 'password': '', 'area': Areas.FIREFIGHTERS, 'city_id': 0, 'department_id': 0}
     department = Department(**data)
     generator.generate(department, True)
+
+    data = {'disaster_name': '', 'department_id': 0}
+    disaster = Disaster(**data)
+    generator.generate(disaster, True)
+
+    data = {'advice': '', 'disaster_id': 0}
+    protocol = Protocol(**data)
+    generator.generate(protocol, True)
+
+    database = Database(launcher)
+
+    """disaster_data = {'disaster_name': 'fire', 'department_id': 1}
+    disaster = Disaster(**disaster_data)
+    disaster = database.save_data(disaster)
+    protocol_data = {'advice': 'evacuate', 'disaster_id': disaster.disaster_id}
+    protocol = Protocol(**protocol_data)
+    database.save_data(protocol)
+    city_data = {'city_name': 'Cochabamba'}
+    city = City(**city_data)
+    city = database.save_data(city)
+
+    department_data = {'user_name': 'police123', 'password': '1234', "area": Areas.POLICE, 'city_id': city.city_id}
+    department = Department(**department_data)
+    database.save_data(department) """
 
 
 if __name__ == '__main__':
