9cbbff13da40f952c4a98bd8b2a25462a34bb524
Sebastian Medrano <sebastian.medrano@fundacion-jala.org>
2019-03-30 04:16:54 -0400

    tables for disaster and protocol added

---
+++ backend/disaster/disaster.py
@@ -0,0 +1,16 @@
+""""Disaster module"""
+from dataclasses import dataclass
+from database.i_table import Table
+
+
+@dataclass
+class Disaster(Table):
+    disaster_name: str
+    department_id: int
+    disaster_id: int = 0
+
+    def get_primary_key(self) -> str:
+        return 'disaster_id'
+
+    def get_class_name(self) -> str:
+        return self.__class__.__name__
