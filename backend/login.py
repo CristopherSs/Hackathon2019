81cb81b9f8c6db229d810a0b145999327507f6dd
Marcandea <Marcandea.Cabrera@fundacion-jala.org>
2019-03-31 09:08:40 -0400

    modified

--- backend/login.py
+++ backend/login.py
@@ -3,10 +3,10 @@ from typing import List
 
 class LoginVerifier:
     """Login Verifier for when someone tries to login"""
-    def __init__(self, existent_users: List) -> None:
+    def __init__(self, existent_users: List)-> None:
         self.__existent_users = existent_users
 
-    def login(self, email: str, password: str) -> bool:
+    def login(self, data_dict:dict) -> bool:
         """
         compares email and password in case the email exists in the database
         :param email: str
@@ -14,8 +14,8 @@ class LoginVerifier:
         :return: bool
         """
         for user_data in self.__existent_users:
-            if user_data['email'] == email:
-                if user_data['password'] == password:
+            if user_data.email == data_dict['email']:
+                if user_data.password == data_dict['password']:
                     return True
                 else:
                     return False
